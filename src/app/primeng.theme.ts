d4ef73834a211f5067c75f91570c24ac68193e7a
Alessandra <alessaconceicao501501@gmail.com>
2025-08-26 21:13:15 -0400

    feat: configurando primeng

---
+++ src/app/primeng.theme.ts
@@ -0,0 +1,5 @@
+import Aura from "@primeng/themes/aura";
+import { definePreset } from "@primeng/themes";
+
+export const AgridoceTranslation = {};
+export const AgridoceTheme =  definePreset(Aura, {});
