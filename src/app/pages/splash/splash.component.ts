f1f0736eb80e3d1af0ffebca722767241bfecf5b
Alessandra <alessaconceicao501501@gmail.com>
2025-08-30 19:16:23 -0400

    feat: adicionado componentes splash, menu e cart

---
+++ src/app/pages/splash/splash.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  imports: [],
+  templateUrl: './splash.component.html',
+  styleUrl: './splash.component.scss'
+})
+export class SplashComponent {
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigateByUrl('/menu');
+    }, 3000);
+  }
+}
