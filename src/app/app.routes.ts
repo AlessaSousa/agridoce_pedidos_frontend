40c47e392e7efa18547e6e0fcab2abe492cd3bf0
Alessandra <alessaconceicao501501@gmail.com>
2025-08-30 19:17:22 -0400

    feat: adicionado componentes no arquivo de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SplashComponent } from './pages/splash/splash.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SplashComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'cart', component: CartComponent },
+    { path: '**', redirectTo: '' }
+];
