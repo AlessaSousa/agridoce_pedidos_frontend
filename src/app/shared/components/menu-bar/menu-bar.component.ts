dcf507bcef66db6724b68e67df1d37bbe8913c2e
Alessandra <alessaconceicao501501@gmail.com>
2025-08-31 18:17:04 -0400

    feat: adicionado barra de navegação

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -3,19 +3,21 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IMenuItems } from '../../models/IMenuItems';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-menu-bar',
-  imports: [
+  imports: [ 
     CommonModule,
     FormsModule,
-    RouterModule
+    RouterModule,
+    MatIconModule
 ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
 export class MenuBarComponent {
   readonly menuItems: WritableSignal<IMenuItems[]> = signal([]);
-  readonly activeIndex: WritableSignal<number> = signal(0);
+  readonly activeIndex: WritableSignal<number> = signal(1);
   ngOnInit() {
     this.menuItems.set([
       {
