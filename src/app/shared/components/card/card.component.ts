45fdfa1fb51d5e54cd69907a7ddd33ab1f5e2b42
Alessandra <alessaconceicao501501@gmail.com>
2025-08-31 18:16:28 -0400

    feat: adicionado componente para os cards no cardápio

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, input, InputSignal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-card',
+  imports: [
+    CommonModule,
+    ButtonModule,
+    FormsModule
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  public card: InputSignal<any | undefined> = input();
+}
