ac3ebed65760a55981b27ccdbd86f288bd5915d0
Alessandra <alessaconceicao501501@gmail.com>
2025-08-31 18:18:04 -0400

    feat: adicionado buttons de categorias e ajustando outros componentes no menu principal

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,21 +1,122 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { MenuBarComponent } from '../../shared/components/menu-bar/menu-bar.component';
 import { InputIcon } from 'primeng/inputicon';
 import { IconField } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-menu',
   imports: [
-    InputIcon, 
-    IconField, 
-    InputTextModule, 
+    InputIcon,
+    IconField,
+    InputTextModule,
     FormsModule,
-    MenuBarComponent
+    MenuBarComponent,
+    ButtonModule,
+    CardComponent,
+    CommonModule
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  public categories: WritableSignal<any[]> = signal([]);
+  public selected: WritableSignal<string> = signal('Bolos');
+  public itemFood: WritableSignal<any[]> = signal([]);
 
+  ngOnInit() {
+    this.categories.set([
+      { nome: 'Bolos', outlined: false },
+      { nome: 'Salgados', outlined: true },
+      { nome: 'Tortas', outlined: true },
+      { nome: 'Bebidas', outlined: true }
+    ])
+    this.itemFood.set([
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      },
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      },
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      },
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      },
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      },
+      {
+        nome: 'Bolo de Morango',
+        valor: 40
+      }, {
+        nome: 'Bolo de Morango',
+        valor: 40
+      }, {
+        nome: 'Bolo de Morango',
+        valor: 40
+      }, {
+        nome: 'Bolo de Morango',
+        valor: 40
+      }
+    ])
+  }
+
+  toogle(event: string) {
+    this.categories.update(categories =>
+      categories.map(cat => ({
+        ...cat,
+        outlined: cat.nome !== event
+      }))
+    )
+
+    if (event === 'Bolos') {
+      const lista = [
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        },
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        },
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        },
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        },
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        },
+        {
+          nome: 'Bolo de Morango',
+          valor: 40
+        }, {
+          nome: 'Bolo de Morango',
+          valor: 40
+        }, {
+          nome: 'Bolo de Morango',
+          valor: 40
+        }, {
+          nome: 'Bolo de Morango',
+          valor: 40
+        }
+      ]
+      this.itemFood.set(lista)
+    }
+    this.selected.set(event);
+  }
 }
