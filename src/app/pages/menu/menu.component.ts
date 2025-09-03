dca252133375ab368b52ce3eccc0d391b8dae371
Alessandra <alessaconceicao501501@gmail.com>
2025-09-02 21:52:41 -0400

    fix: corregido listagem dos items

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -7,6 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardComponent } from '../../shared/components/card/card.component';
 import { CommonModule } from '@angular/common';
+
+interface ICategories {
+  nome: string;
+  outlined: boolean;
+}
+interface IItems {
+  nome: string;
+  valor: number;
+  image?: string;
+}
 @Component({
   selector: 'app-menu',
   imports: [
@@ -23,9 +33,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
-  public categories: WritableSignal<any[]> = signal([]);
+  public categories: WritableSignal<ICategories[]> = signal([]);
   public selected: WritableSignal<string> = signal('Bolos');
-  public itemFood: WritableSignal<any[]> = signal([]);
+  public itemFood: WritableSignal<IItems[]> = signal([]);
 
   ngOnInit() {
     this.categories.set([
@@ -34,50 +44,14 @@ export class MenuComponent {
       { nome: 'Tortas', outlined: true },
       { nome: 'Bebidas', outlined: true }
     ])
-    this.itemFood.set([
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      },
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      },
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      },
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      },
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      },
-      {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      }, {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      }, {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      }, {
-        nome: 'Bolo de Morango',
-        valor: 40,
-        image: 'assets/menu/bolo_chocolate_3.jpeg'
-      }
-    ])
+
+    const itemBase = {
+      nome: 'Bolo de Morango',
+      valor: 40,
+      image: 'assets/menu/bolo_chocolate_3.jpeg'
+    }
+    const lista = Array.from({ length: 8 }, () => ({ ...itemBase }));
+    this.itemFood.set(lista)
   }
 
   toogle(event: string) {
@@ -89,50 +63,37 @@ export class MenuComponent {
     )
 
     if (event === 'Bolos') {
-      const lista = [
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        },
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        },
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        },
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        },
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        },
-        {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        }, {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        }, {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        }, {
-          nome: 'Bolo de Morango',
-          valor: 40,
-          image: 'assets/menu/bolo_chocolate_3.jpeg'
-        }
-      ]
+      const itemBase = {
+        nome: 'Bolo de Morango',
+        valor: 40,
+        image: 'assets/menu/bolo_chocolate_3.jpeg'
+      }
+      const lista = Array.from({ length: 8 }, () => ({ ...itemBase }));
+      this.itemFood.set(lista)
+
+    } else if (event === 'Salgados') {
+      const itemBase = {
+        nome: 'Salgado de presunto',
+        valor: 40,
+        image: 'assets/menu/salgado_jacare.jpeg'
+      }
+      const lista = Array.from({ length: 8 }, () => ({ ...itemBase }));
+      this.itemFood.set(lista);
+
+    } else if (event === 'Tortas') {
+      const itemBase = {
+        nome: 'Torta de Chocolate',
+        valor: 40,
+        image: 'assets/menu/torta_chocolate.jpeg'
+      }
+      const lista = Array.from({ length: 8 }, () => ({ ...itemBase }))
+      this.itemFood.set(lista)
+    } else {
+      const itemBase = {
+        nome: '-------------',
+        valor: 0,
+      }
+      const lista = Array.from({ length: 8 }, () => ({ ...itemBase }))
       this.itemFood.set(lista)
     }
     this.selected.set(event);
