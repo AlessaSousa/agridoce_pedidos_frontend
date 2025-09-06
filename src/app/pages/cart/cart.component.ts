5cace1827fa4fa8c0ca44a2742962b0ecad873c8
Alessandra <alessaconceicao501501@gmail.com>
2025-09-06 19:05:49 -0400

    feat: adicionado componentes para estrutura do carrinho

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, signal, WritableSignal } from '@angular/core';
+import { ItemComponent } from '../../shared/components/item/item.component';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DialogFormComponent } from '../../shared/components/dialog-form/dialog-form.component';
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [
+    ItemComponent,
+    ButtonModule,
+    DialogFormComponent
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
+  public visible: WritableSignal<boolean> = signal(false);
 
+  showModal() {
+    this.visible.set(true);
+  }
+
+  closeDialog() {
+    this.visible.set(false);
+  }
 }
