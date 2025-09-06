d129f2c5dfba16d3feb51bb23c01491f2c8058f7
Alessandra <alessaconceicao501501@gmail.com>
2025-09-06 19:04:55 -0400

    feat: adicionado componente para detalhes do item card

---
+++ src/app/pages/item-details/item-details.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputNumber } from 'primeng/inputnumber';
+import { Button, ButtonModule } from "primeng/button";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-item-details',
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputNumber,
+    ButtonModule,
+],
+  templateUrl: './item-details.component.html',
+  styleUrl: './item-details.component.scss'
+})
+export class ItemDetailsComponent {
+  private router = inject(Router);
+  public detail: WritableSignal<any | null> = signal(null);
+  
+  ngOnInit() {
+    this.detail.set(
+      {
+        title: 'Bolo de Chocolate Vulcão',
+        subtitle: 'Serve 8 à 10 pessoas.',
+        ingrediente: 'trigo, chocolate em pó, fermento, leite condensado, margarina, ovos, leite integral',
+        valor: 100,
+        quantidade: 2,
+        image: 'assets/menu/bolo_chocolate_3.jpeg'
+      }
+    )
+  }
+
+  goToMenu() {
+    this.router.navigate(['/menu'])
+  }
+  goToCart() {
+    this.router.navigate(['/cart'])
+  }
+}
