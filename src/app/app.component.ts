41f74f3b330b4eb7664bbed8b0602ff76e31b6be
Alessandra <alessaconceicao501501@gmail.com>
2025-09-06 19:07:56 -0400

    feat: adicionado lógica para visulizar menu-bar em rotas específicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MenuBarComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private router = inject(Router);
   title = 'agridoce_pedidos';
+  public showNavBar: WritableSignal<boolean> = signal(false);
+
+  constructor() {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        this.showNavBar.set(['/menu', '/cart'].includes(event.urlAfterRedirects))
+      }
+    })
+  }
 }
