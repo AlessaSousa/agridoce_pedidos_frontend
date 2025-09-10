b10ab47687bf2e61e1f7c30bd46fbc391a1da03a
Alessandra <alessaconceicao501501@gmail.com>
2025-09-09 20:52:29 -0400

    feat: adicionado reactiveForms

--- src/app/shared/components/dialog-form/dialog-form.component.ts
+++ src/app/shared/components/dialog-form/dialog-form.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputMaskModule } from 'primeng/inputmask';
 import { SelectModule } from 'primeng/select';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-form',
@@ -18,19 +19,44 @@ import { SelectModule } from 'primeng/select';
     FormsModule,
     FloatLabel,
     InputMaskModule,
-    SelectModule
+    SelectModule,
+    ReactiveFormsModule
   ],
   templateUrl: './dialog-form.component.html',
   styleUrl: './dialog-form.component.scss'
 })
 export class DialogFormComponent {
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
   public visible: InputSignal<boolean> = input.required();
   public visibleChange: OutputEmitterRef<boolean> = output();
   public type: WritableSignal<string> = signal('user');
-  public typePayment: WritableSignal<string[]> = signal(['Dinheiro, Cartão de Crédito, Cartão de Débito, Pix'])
-  selectedPayment = '';
-  value1 = '';
-  value2 = '';
+  public typePayment: WritableSignal<any[]> = signal([]);
+  public total: WritableSignal<number> = signal(110);
+  public formUser: FormGroup;
+
+  constructor() {
+    this.formUser = this.formBuilder.group({
+      nome: [null, [Validators.required]],
+      telefone: [null, [Validators.required]],
+      rua: [null, [Validators.required]],
+      numero: [null, [Validators.required]],
+      bairro: [null, [Validators.required]],
+      cep: [null, [Validators.required]],
+      metodo_pgto: [null, [Validators.required]]
+    })
+  }
+
+  ngOnInit() {
+    this.typePayment.set([
+      { name: 'Dinheiro' },
+      { name: 'Pix' },
+      { name: 'Crédito' },
+      { name: 'Débito' }
+    ])
+  }
+
   get _visible(): boolean {
     return this.visible();
   }
@@ -45,6 +71,12 @@ export class DialogFormComponent {
 
   advance(type: string) {
     this.type.set(type);
+    if (type === 'finalizar') {
+      this.closeDialog()
+      this.router.navigate(['/final'])
+      console.log('Dados usuário salvo', this.formUser.value)
+    }
+
   }
 
 }
