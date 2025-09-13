2e99b875c3d7290f9c67901478d63af49e18628f
Alessandra <alessaconceicao501501@gmail.com>
2025-09-12 20:27:52 -0400

    feat: adicionado componente para login e register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { CompletedComponent } from './pages/completed/completed.component';
+import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
     { path: '', component: SplashComponent },
     { path: 'menu', component: MenuComponent },
     { path: 'cart', component: CartComponent },
     { path: 'detail', component: DetailsComponent },
     { path: 'final', component: CompletedComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' }
 ];
