c6281cdf676f4269a3830793c4e842ece700f942
Alessandra <alessandra.projex@gmail.com>
2025-09-24 13:19:09 -0400

    tentativa falha de fazer logout

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  logout() {
+    localStorage.setItem('isLogged', 'false')
+  }
 }
